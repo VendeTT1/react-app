ab661968585c7256b1b73102d83a5f5e24acb70f
Tanjaoui Nasreddine <nasreddine.tanjaoui-etu@etu.univh2c.ma>
2024-03-29 21:04:54 +0000

    add login page

--- src/App.js
+++ src/App.js
@@ -11,12 +11,8 @@ function App() {
         <div className="auth-wrapper">
           <div className="auth-inner">
             <Routes>
-<<<<<<< HEAD
               <Route exact path="/" element={<Login />} />
               <Route path="/react-app" element={<Login />} />
-=======
-              <Route path="/sign-in" element={<Login />} />
->>>>>>> e06d2d95f5814911012134cf30fe5b04136f9eb7
 
             </Routes>
           </div>
